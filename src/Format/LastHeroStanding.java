02b34f9934770e92a211b64fdae8ce571018d837
BoraxTheClean <owen7770210@gmail.com>
2016-08-26 23:53:56 -0500

    Removed code from LHS.

--- src/Format/LastHeroStanding.java
+++ src/Format/LastHeroStanding.java
@@ -2,24 +2,8 @@ import java.util.Random;
 public class LastHeroStanding implements Format{
 
 	public Player play(Player playerOne,Player playerTwo,int wins){
-		Random rng=new Random();
-		int oneDecks=0;
-		int twoDecks=0;
-		int oneWins=0;
-		int twoWins=0;
-		int winner=0;
-		while(oneWins!= wins || twoWins!=wins){
-			//Choose decks
-			boolean ready=true;
-			if(winner==0){
-				
-			}
-			while(ready){
-				
 
-			}
 
-		}
 
 	}
 }
