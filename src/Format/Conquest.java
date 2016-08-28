c55d37150778e70e9b52949f0b353c4106b72881
Harvey Chen <harvc015@gmail.com>
2016-08-28 16:49:05 -0400

    Fixed Conquest and Updated LastHeroStanding

--- src/Format/Conquest.java
+++ src/Format/Conquest.java
@@ -6,8 +6,8 @@ public class Conquest implements Format{
 	public Player play(Player playerOne,Player playerTwo){		
 		Player winner;
 		while(playerOne.hasDecks() || playerTwo.hasDecks()){
-			Deck playerOneDeck = playerOne.getUnusedDeck;
-			Deck playerTwoDeck = playerTwo.getUnusedDeck;
+			Deck playerOneDeck = playerOne.getUnusedDeck();
+			Deck playerTwoDeck = playerTwo.getUnusedDeck();
 			
 			float playerOneWin = playerOneDeck.getWinPercentage(playerTwoDeck);
 			
@@ -28,6 +28,8 @@ public class Conquest implements Format{
 		else{
 			winner = playerOne;
 		}
+		playerOne.resetDecks();
+		playerTwo.resetDecks();
 		
 		return winner;
 	}
--- src/Format/LastHeroStanding.java
+++ src/Format/LastHeroStanding.java
@@ -1,9 +1,47 @@
 import java.util.Random;
+import Player.Player;
+
 public class LastHeroStanding implements Format{
-
-	public Player play(Player playerOne,Player playerTwo,int wins){
-
-
-
+	
+	public Player play(Player playerOne,Player playerTwo){		
+		Player winner;
+		Deck playerOneDeck;
+		Deck playerTwoDeck;
+		
+		while(playerOne.hasDecks() || playerTwo.hasDecks()){
+			if(playerOne.isActive){
+				playerOneDeck = playerOne.getUnusedDeck();
+			}
+			if(playerTwo.isActive){
+				playerTwoDeck = playerTwo.getUnusedDeck();
+			}
+			
+			float playerOneWin = playerOneDeck.getWinPercentage(playerTwoDeck);
+			
+			Random rng = new Random();
+			float playerTwoWin = rng.nextFloat();
+			
+			if(playerOneWin > playerTwoWin){
+				playerTwo.setDeckToUsed(playerTwoDeck);
+				playerOne.isActive = false;
+				playerTwo.isActive = true;
+			}
+			else{
+				playerOne.setDeckToUsed(playerOneDeck);
+				playerOne.isActive = true;
+				playerTwo.isActive = false;
+			}
+		}
+		
+		if(playerTwo.hasDecks()){
+			winner = playerTwo;
+		}
+		else{
+			winner = playerOne;
+		}
+		playerOne.resetDecks();
+		playerTwo.resetDecks();
+		
+		return winner;
 	}
 }
