6d75ed8f1ebef11f6d7e79e8e2a8655117ceaf3c
Harvey Chen <harvc015@gmail.com>
2016-08-28 04:13:06 -0400

    Added Conquest

---
+++ src/Format/Conquest.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import Player.Player;
+
+public class Conquest implements Format{
+	
+	public Player play(Player playerOne,Player playerTwo){		
+		Player winner;
+		while(playerOne.hasDecks() || playerTwo.hasDecks()){
+			Deck playerOneDeck = playerOne.getUnusedDeck;
+			Deck playerTwoDeck = playerTwo.getUnusedDeck;
+			
+			float playerOneWin = playerOneDeck.getWinPercentage(playerTwoDeck);
+			
+			Random rng = new Random();
+			float playerTwoWin = rng.nextFloat();
+			
+			if(playerOneWin > playerTwoWin){
+				playerOne.setDeckToUsed(playerOneDeck);
+			}
+			else{
+				playerTwo.setDeckToUsed(playerTwoDeck);
+			}
+		}
+		
+		if(playerOne.hasDecks()){
+			winner = playerTwo;
+		}
+		else{
+			winner = playerOne;
+		}
+		
+		return winner;
+	}
+}
