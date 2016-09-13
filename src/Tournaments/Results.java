760f13be93452ddb03b261b08d92fb0a833d8730
Harvey Chen <harvc015@gmail.com>
2016-09-13 11:16:20 -0400

    Attempt to fix updatePlacementPathMap

--- src/Tournaments/Results.java
+++ src/Tournaments/Results.java
@@ -4,25 +4,17 @@ import java.util.*;
 public class Results{
 	/*
 	"I would have stored data on each player, their lineup, their path through the bracket including match/game results, and their finish
-
 Tracking lineup: A trie structure would work best for retrieval of data.  But this is not efficient for creation and updating.  We could have a list of deck id's, then we would need to make sure these are normalized (sorted) so they would all hash to the same bucket.  We can't have [1,2,3] and [2,1,3].
-
 Need to be able to index by player id and by placement in tournament as well.  Each of these should be done with maps, so we will need the following maps:
-
 Player Placement Map<Place in tournament,Map<Players id,# of times finished>>
 Player id Map<Player id,Map<Place,times>>
 Player path Map<Player id, List<List<Matches>><
 Player path Map<Place in tournament, List<List<Matches>>
-
 and you could index by player ID and also provide an index by placement
-
 so you could ask, what were the top 8 lineups
-
 or did player 112 make top 8
-
 or how many players brought zoo and how many players with zoo made top 8
 "
-
 	*/
 	private Map<Integer,HashMap<Integer,Integer>> placementMap=new HashMap<>();
 	private Map<Integer,HashMap<Integer,Integer>> playerMap=new HashMap<>();
@@ -35,7 +27,6 @@ or how many players brought zoo and how many players with zoo made top 8
 		The idea here is to have the maps be set with the data I want
 		so I'll never have to insert into the maps, all the buckets
 		will be there on initialization.
-
 	*/
 
 	public Map<Integer,Player> getPlayers(){
@@ -44,20 +35,23 @@ or how many players brought zoo and how many players with zoo made top 8
 	public Map<Integer,Integer> getPlayerRankings(){
 		return playerRankings;
 	}
+	public Map<Integer,HashMap<Integer,Integer>> getPlayerMap(){
+		return playerMap;
+	}
+	public Map<Integer,HashMap<Integer,Integer>> getPlacementMap(){
+		return placementMap;
+	}
+	public Map<Integer,List<List<Match>>> getPlacementPathMap(){
+		return playerPlacementPath;
+	}
+	
 	public void setPlacementMap(int i){
 		if(placementMap.get(i)==null)
 			placementMap.put(i,new HashMap<Integer,Integer>());
 	}
-
-	public Map<Integer,HashMap<Integer,Integer>> getPlacementMap(){
-		return placementMap;
-	}	
 	public void setPlayerMap(int i){
 		if(playerMap.get(i)==null)
 			playerMap.put(i,new HashMap<Integer,Integer>());
-	}
-	public Map<Integer,HashMap<Integer,Integer>> getPlayerMap(){
-		return playerMap;
 	}
 	public void setPlayerPathMap(int i){
 		if(playerIDPath.get(i)==null)
@@ -70,9 +64,7 @@ or how many players brought zoo and how many players with zoo made top 8
 		if(playerPlacementPath.get(i)==null)
 			playerPlacementPath.put(i,new LinkedList<List<Match>>());
 	}
-	public Map<Integer,List<List<Match>>> getPlacementPathMap(){
-		return playerPlacementPath;
-	}
+	
 	/*
 		I think I want to update the maps with data using a 
 		Map<Ranking,List<Player>> thus I can just iterate over the keyset
@@ -114,12 +106,14 @@ or how many players brought zoo and how many players with zoo made top 8
 	public void updatePlacementPathMap(){
 		for(Integer i: playerPlacementPath.keySet()){
 			List<List<Match>> list=playerPlacementPath.get(i);
-			for(Player p: results.get(i)){
-				if(list==null){
-					list=new LinkedList<List<Match>>();
-					list.add(p.history);
-				}else{
-					list.add(p.history);
+			if(results.containsKey(i)){
+				for(Player p: results.get(i)){
+					if(list==null){
+						list=new LinkedList<List<Match>>();
+						list.add(p.history);
+					}else{
+						list.add(p.history);
+					}
 				}
 			}
 		}
@@ -134,5 +128,3 @@ or how many players brought zoo and how many players with zoo made top 8
 		}
 	}
 }
-
-
